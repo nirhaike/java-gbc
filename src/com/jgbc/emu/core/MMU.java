53a71f3acc39467dd0c9f23a3880bf3995c384bb
nirhaike <niruser12@gmail.com>
2016-04-18 16:01:36 +0300

    added constants

--- src/com/jgbc/emu/core/MMU.java
+++ src/com/jgbc/emu/core/MMU.java
@@ -11,51 +11,51 @@ package com.jgbc.emu.core;
  */
 public class MMU {
 
-	public static final int SB		= 0xFF01;
-	public static final int SC		= 0xFF02;
-	public static final int DIV		= 0xFF04;
-	public static final int TIMA	= 0xFF05;
-	public static final int TMA		= 0xFF06;
-	public static final int TAC		= 0xFF07;
+	public static final int SB = 0xFF01;
+	public static final int SC = 0xFF02;
+	public static final int DIV = 0xFF04;
+	public static final int TIMA = 0xFF05;
+	public static final int TMA = 0xFF06;
+	public static final int TAC = 0xFF07;
 	
-	public static final int IF		= 0xFF0F; // interrupt flag
+	public static final int IF = 0xFF0F; // interrupt flag
 	
-	public static final int NR10	= 0xFF10; // sound mode 1 register
-	public static final int NR11	= 0xFF11;
-	public static final int NR12	= 0xFF12;
-	public static final int NR13	= 0xFF13;
-	public static final int NR14	= 0xFF14;
-	public static final int NR21	= 0xFF16;
-	public static final int NR22	= 0xFF17;
-	public static final int NR23	= 0xFF18;
-	public static final int NR24	= 0xFF19;
-	public static final int NR30	= 0xFF1A;
-	public static final int NR31	= 0xFF1B;
-	public static final int NR32	= 0xFF1C;
-	public static final int NR33	= 0xFF1D;
-	public static final int NR34	= 0xFF1E;
-	public static final int NR41	= 0xFF20;
-	public static final int NR42	= 0xFF21;
-	public static final int NR43	= 0xFF22;
-	public static final int NR44	= 0xFF23;
-	public static final int NR50	= 0xFF24;
-	public static final int NR51	= 0xFF25;
-	public static final int NR52	= 0xFF26; // all sound on/off
-	public static final int[] WAVE	= new int[]{0xFF30, 0xFF3F}; // wave pattern ram
+	public static final int NR10 = 0xFF10; // sound mode 1 register
+	public static final int NR11 = 0xFF11;
+	public static final int NR12 = 0xFF12;
+	public static final int NR13 = 0xFF13;
+	public static final int NR14 = 0xFF14;
+	public static final int NR21 = 0xFF16;
+	public static final int NR22 = 0xFF17;
+	public static final int NR23 = 0xFF18;
+	public static final int NR24 = 0xFF19;
+	public static final int NR30 = 0xFF1A;
+	public static final int NR31 = 0xFF1B;
+	public static final int NR32 = 0xFF1C;
+	public static final int NR33 = 0xFF1D;
+	public static final int NR34 = 0xFF1E;
+	public static final int NR41 = 0xFF20;
+	public static final int NR42 = 0xFF21;
+	public static final int NR43 = 0xFF22;
+	public static final int NR44 = 0xFF23;
+	public static final int NR50 = 0xFF24;
+	public static final int NR51 = 0xFF25;
+	public static final int NR52 = 0xFF26; // all sound on/off
+	public static final int[] WAVE = new int[]{0xFF30, 0xFF3F}; // wave pattern ram
 	
-	public static final int LCDC	= 0xFF40;
-	public static final int STAT	= 0xFF41;
-	public static final int SCY		= 0xFF42;
-	public static final int SCX		= 0xFF43;
-	public static final int LY		= 0xFF44;
-	public static final int LYC		= 0xFF45;
-	public static final int DMA		= 0xFF46;
-	public static final int BGP		= 0xFF47;
-	public static final int OBP0	= 0xFF48;
-	public static final int OBP1	= 0xFF49;
-	public static final int WY		= 0xFF4A;
-	public static final int WX		= 0xFF4B;
-	public static final int IE		= 0xFFFF; // interrupt enable flag
+	public static final int LCDC = 0xFF40;
+	public static final int STAT = 0xFF41;
+	public static final int SCY = 0xFF42;
+	public static final int SCX = 0xFF43;
+	public static final int LY = 0xFF44;
+	public static final int LYC = 0xFF45;
+	public static final int DMA = 0xFF46;
+	public static final int BGP = 0xFF47;
+	public static final int OBP0 = 0xFF48;
+	public static final int OBP1 = 0xFF49;
+	public static final int WY = 0xFF4A;
+	public static final int WX = 0xFF4B;
+	public static final int IE = 0xFFFF; // interrupt enable flag
 	
 	
 	private Cartridge cart;

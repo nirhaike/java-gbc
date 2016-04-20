c45ac5154b78ceef8e47a34f4a46a5ecb472882e
nirhaike <niruser12@gmail.com>
2016-04-20 22:30:51 +0300

    updated write byte structure

--- src/com/jgbc/emu/core/MMU.java
+++ src/com/jgbc/emu/core/MMU.java
@@ -155,7 +155,39 @@ public class MMU {
 	
 	public void writeByte(int address, int value) {
 		switch (address & 0xF000) {
-		default:
+		// bios
+		case 0x0000:
+			if (inBios) {
+				break;
+			}
+			break;
+		// rom0
+		case 0x1000:
+		case 0x2000:
+		case 0x3000:
+			break;
+		// rom1 (unbanked)
+		case 0x4000:
+		case 0x5000:
+		case 0x6000:
+		case 0x7000:
+			break;
+		// vram (graphics)
+		case 0x8000:
+		case 0x9000:
+			break;
+		// external ram
+		case 0xA000:
+		case 0xB000:
+			break;
+		// working ram
+		case 0xC000:
+		case 0xD000:
+		// working ram shadow
+		case 0xE000:
+			break;
+		// working ram shadow, I/O, zero-page ram
+		case 0xf000:
 			break;
 		}
 	}
